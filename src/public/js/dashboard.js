10e644a1ec440c1a61d75125a9b7cc9d7d290df1
samuel-poinama <samuel.poinama@efrei.net>
2025-04-03 17:17:49 +0200

    link objetives

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,4 +1,29 @@
-const caloriesBar = document.getElementById("calories-bar");
-const proteinBar = document.getElementById("protein-bar");
-const fatBar = document.getElementById("fat-bar");
-fatBar.style.width = "0%";
+async function getStats() {
+    const token = localStorage.getItem('token');
+
+    const requestObj = await fetch('/api/objective', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    });
+
+    const responseObj = await requestObj.json();
+
+    const caloriesText = document.getElementById("calories-text");
+    const proteinText = document.getElementById("protein-text");
+    const fatText = document.getElementById("fat-text");
+
+    caloriesText.innerHTML = responseObj.calories;
+    proteinText.innerHTML = responseObj.protein;
+    fatText.innerHTML = responseObj.fat;
+
+    const caloriesBar = document.getElementById("calories-bar");
+    const proteinBar = document.getElementById("protein-bar");
+    const fatBar = document.getElementById("fat-bar");
+    fatBar.style.width = "0%";
+
+}
+
+getStats();
--- src/public/js/objective.js
+++ src/public/js/objective.js
@@ -1,6 +1,51 @@
-document.getElementById('objectiveForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+
+
+async function getObjective() {    
+    const token = localStorage.getItem('token');
+
+    const request = await fetch('/api/objective', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    });
+
+    if (request.status === 200) {
+        const response = await request.json();
+        document.getElementById('calories').value = response.calories;
+        console.log(token);
+        document.getElementById('fat').value = response.fat;
+        document.getElementById('proteins').value = response.protein;
+    }
+    else {
+        window.location.href = '/login';
+    }
+}
+
+getObjective();
+
+async function setObjective() {
+    console.log('setObjective called');
+    const token = localStorage.getItem('token');
+
     const calories = document.getElementById('calories').value;
     const fat = document.getElementById('fat').value;
-    alert(`Objective saved!\nCalories: ${calories} kcal\nFat: ${fat} g`);
-});
+    const protein = document.getElementById('proteins').value;
+
+    const data = { calories, fat, protein };
+
+    const request = await fetch('/api/objective', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        body: JSON.stringify(data)
+    });
+
+    console.log(request);
+    if (request.status === 200) {
+        await getObjective();
+    }
+}
